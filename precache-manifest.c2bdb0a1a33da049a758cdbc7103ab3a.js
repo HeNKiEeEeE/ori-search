d527be77d69713fd0d2e8d59f8bb1d90b7676994
Hendrik Grondijs <hendrik@stamkracht.com>
2019-08-15 17:19:43 +0200

    Updates

---
+++ precache-manifest.c2bdb0a1a33da049a758cdbc7103ab3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc66fb16bae8b5ca3f85cc0fb162df8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "766e9564c5067c4e7180",
+    "url": "/static/css/main.6510d9fb.chunk.css"
+  },
+  {
+    "revision": "05ef8cb8a467bd486cb5",
+    "url": "/static/js/2.77efceac.chunk.js"
+  },
+  {
+    "revision": "766e9564c5067c4e7180",
+    "url": "/static/js/main.f3a61bf3.chunk.js"
+  },
+  {
+    "revision": "4b87e531fa7a856afd34",
+    "url": "/static/js/runtime~main.350d25de.js"
+  },
+  {
+    "revision": "8740973f0f01e90d33bfb856b5ee758c",
+    "url": "/static/media/vnglogo.8740973f.svg"
+  }
+]);
